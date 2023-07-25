6cae8f96e7fe0a75d0a23fb4e30739081ee35535
Moizhassnat <moizhassnat10@gmail.com>
2023-07-25 19:43:06 +0500

    Email setting

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_NAME", "Devxel" );
+define( "RECIPIENT_EMAIL", "info@devxel.co" );
 
 // Read the form values
 $success = false;
